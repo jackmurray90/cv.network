32e92caf17136dee217ff71d15bc49460e5bb629
Jack Murray <jack@cv.network>
2023-02-01 13:27:21 +0000

    add text role to future string

--- translations/en/strings.py
+++ translations/en/strings.py
@@ -28,7 +28,7 @@ strings = {
   'classes_comma': "Classes/Projects (comma separated)",
   'awards': "Awards/Scholarships",
   'classes': "Classes/Projects",
-  'future': "Future",
+  'future': "Future role",
   'edit_basic_info': "Edit basic info",
   'edit_experience': "Edit this work experience",
   'new_experience': "Add a new work experience",
